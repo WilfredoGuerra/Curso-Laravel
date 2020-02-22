48b839de3d26b938e182bc70538799379eddf5df
WilfredoGuerra <willigue@gmail.com>
2020-02-22 09:45:59 -0500

    implementacion del codigo Admin LTE a Blade

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Facade\FlareClient\View;
 use Illuminate\Support\ServiceProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View()->share('theme', 'lte');
+        //View::share('theme', 'lte');
     }
 }
