ff8479c99398ead37203a921f2be6e9034e29788
WilfredoGuerra <willigue@gmail.com>
2020-02-28 20:39:05 -0500

    Clase 15 mensaje desde el controlador

--- resources/views/admin/menu/form.blade.php
+++ resources/views/admin/menu/form.blade.php
@@ -1,20 +1,20 @@
 <div class="form-group">
     <label for="nombre" class="col-lg-2 control-label requerido">Nombre</label>
         <div class="col-sm-8">
-            <input type="text" name="nombre" id="nombre" value="" class="form-control" required>
+            <input type="text" name="nombre" id="nombre" value="{{old('nombre')}}" class="form-control">
         </div>
 </div>
 
 <div class="form-group">
     <label for="url" class="col-lg-2 control-label requerido">Url</label>
         <div class="col-sm-8">
-            <input type="text" name="url" id="url" class="form-control" value="" required>
+            <input type="text" name="url" id="url" class="form-control" value="{{old('url')}}" required>
         </div>
 </div>
 
 <div class="form-group">
     <label for="icono" class="col-lg-2 control-label">Icono</label>
         <div class="col-sm-8">
-            <input type="text" name="icono" id="icono" class="form-control" value="">
+            <input type="text" name="icono" id="icono" class="form-control" value="{{old('icono')}}">
         </div>
 </div>
